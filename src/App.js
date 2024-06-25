dcd90ac9b704fbe5c9c22fe4ccb5d32825e2c8dc
Amazon <mishoxuco2001@gmail.com>
2024-06-25 13:42:45 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+class App extends Component {
+  state = {
+    inputValue: "",
+    unfulfilledTasks: [
+      { task: "create React project", id: 1 },
+      { task: "create Other project", id: 2 },
+    ],
+    completeTasks: [],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  onChange = (event) => {
+    const value = event.target.value;
+    this.setState({
+      inputValue: value,
+    });
+  };
+
+  addTask = (event) => {
+    event.preventDefault();
+
+    const unfulfilledTask = {
+      id: this.state.unfulfilledTasks.length + 1,
+      task: this.state.inputValue,
+    };
+    this.setState({
+      unfulfilledTasks: [...this.state.unfulfilledTasks, unfulfilledTask],
+      inputValue: "",
+    });
+  };
+
+  deleteTask = (id) => {
+    const deleteTask = this.state.completeTasks.filter(
+      (task) => task.id !== id
+    );
+    this.setState({
+      completeTasks: deleteTask,
+    });
+  };
+  completeTasks = (id) => {
+    const complete = {
+      id: this.state.completeTasks.length + 1,
+      task: this.state.unfulfilledTasks.id,
+    };
+    this.setState({
+      completeTasks: [...this.state.completeTasks, complete],
+    });
+  };
+  undo = (id) => {
+    const undo = {
+      id: this.state.unfulfilledTasks.length + 1,
+      task: this.state.unfulfilledTasks.id,
+    };
+    this.setState({
+      unfulfilledTasks: [...this.state.unfulfilledTasks, undo],
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.addTask} className="add-task">
+          <input
+            type="text"
+            placeholder="enret task..."
+            onChange={this.onChange}
+            value={this.state.inputValue}
+          />
+          <button type="submit">Add Task</button>
+        </form>
+        <div className="tasks">
+          <div className="unfulfilled">
+            <h3>unfulfilled Task</h3>
+            <div className="unfulfilled-task">
+              {this.state.unfulfilledTasks.map((task) => (
+                <div key={task.id}>
+                  <p>
+                    ID: {task.id} TASK: {task.task}
+                  </p>
+
+                  <button onClick={() => this.completeTasks(task.id)}>
+                    Finish
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="done">
+            <h3>Done Task</h3>
+            <div className="done-task">
+              {this.state.completeTasks.map((task) => (
+                <div key={task.id}>
+                  <p>
+                    ID: {task.id} TASK: {task.task}
+                  </p>
+                  <button onClick={() => this.deleteTask(task.id)}>
+                    Delete
+                  </button>
+                  <button onClick={() => this.undo(task.id)}>
+                    undo in unfulfilled
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
